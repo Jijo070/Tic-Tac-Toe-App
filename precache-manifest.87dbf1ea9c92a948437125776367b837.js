5977b74f7749b327e4f693354d5b3347dcb591fe
Jijo070 <jijo.johny07@gmail.com>
2018-10-22 23:17:44 -0400

    Updates

---
+++ precache-manifest.87dbf1ea9c92a948437125776367b837.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb91bcaec2ba7c769a32f2d455f935ff",
+    "url": "/Tic-Tac-Toe-App/static/media/square.eb91bcae.svg"
+  },
+  {
+    "revision": "585bd2a4f9748a66c26323ffaaaac108",
+    "url": "/Tic-Tac-Toe-App/static/media/connery.585bd2a4.svg"
+  },
+  {
+    "revision": "b1f5ffeea82560acc711",
+    "url": "/Tic-Tac-Toe-App/static/js/runtime~main.b1f5ffee.js"
+  },
+  {
+    "revision": "c06478f658bc181c5d58",
+    "url": "/Tic-Tac-Toe-App/static/js/main.c06478f6.chunk.js"
+  },
+  {
+    "revision": "36afbc2ff7a701f5e8ee",
+    "url": "/Tic-Tac-Toe-App/static/js/1.36afbc2f.chunk.js"
+  },
+  {
+    "revision": "c06478f658bc181c5d58",
+    "url": "/Tic-Tac-Toe-App/static/css/main.bd568493.chunk.css"
+  },
+  {
+    "revision": "e88e5396b080c30e7ae58749dd6a1503",
+    "url": "/Tic-Tac-Toe-App/index.html"
+  }
+];
